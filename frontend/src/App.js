745f98495a4df417f6066c61901d7d1b72186c47
Shreyas C R <chaluvapathishreyas@gmail.com>
2026-01-20 18:55:40 +0530

    Fix git tracking: remove submodule, clean macOS files

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Overview from "./pages/Overview";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>
+        Preliminary Statistical Analysis of Web Series Dataset
+      </h1>
+      <Overview />
+    </div>
+  );
+}
+
+export default App;
