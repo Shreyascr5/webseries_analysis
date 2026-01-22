792e033b66576c6e6d83937135c92ba9fbeec0f5
Shreyas C R <chaluvapathishreyas@gmail.com>
2026-01-22 23:04:57 +0530

    Final commit

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-const API_BASE_URL = "http://127.0.0.1:5000";
-
-export async function getOverview() {
-  const response = await fetch(`${API_BASE_URL}/overview`);
-  if (!response.ok) {
-    throw new Error("Failed to fetch overview data");
-  }
-  return response.json();
-}
